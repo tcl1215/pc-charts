5b5f6d019455d834876d9d92f4f9dca04c4b1aa1
liuzhan <549112374@qq.com>
2017-10-24 15:25:02 +0800

    add chart 1

--- js/chart1.js
+++ js/chart1.js
@@ -1,4 +1,69 @@
 // JavaScript Document
 $(document).ready(function(){
-    
+    showCharts();
 })
+
+function showCharts() {
+    var myChart = echarts.init(document.getElementById('main'));
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            left: 'center',
+            data: ['分值'],
+            textStyle: {color: '#fff'},
+            itemWidth: 50
+        },
+        xAxis: {
+            type: 'category',
+            data: ['8.1', '8.2', '8.3', '8.4', '8.5', '8.6', '8.7'],
+            axisLine: {
+                lineStyle: {color: '#fff'}
+            },
+            splitLine: {
+                show: true,
+                lineStyle: {color: '#fff'}
+            },
+            axisLabel: {
+                textStyle: {fontSize: 32}
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        yAxis: {
+            type: 'value',
+            min: 80,
+            max: 100,
+            axisLine: {
+                lineStyle: {color: '#fff'}
+            },
+            splitLine: {
+                lineStyle: {color: '#fff'}
+            },
+            axisLabel: {
+                textStyle: {fontSize: 32}
+            }
+        },
+        series: [
+            {
+                name: '分值',
+                type: 'line',
+                data: [90,97,87,98,89,84,92],
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'top',
+                        textStyle: {color: '#fff', fontSize: 24}
+                    }
+                },
+            }
+        ],
+        color: ['#0000ff']
+    };
+    myChart.setOption(option);
+}
