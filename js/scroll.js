d57495a728a09606d6c07778fb980d5beeae3f9c
liuzhan <549112374@qq.com>
2017-11-17 22:30:57 +0800

    修改滚动

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,8 @@
 // JavaScript Document
 $(document).ready(function(){
 	$('.page2 .left-count').slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:6,interTime:50,trigger:"click"});
-    $('.page2 .info-box').slide({mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:5});
+    $('.page2 .info-box').slide({mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:4});
     
-    $('.page3 .info-box').slide({mainCell:".bd2 ul",autoPage:true,effect:"top",autoPlay:true,vis:5});
+    $('.page3 .info-box').slide({mainCell:".bd2 ul",autoPage:true,effect:"top",autoPlay:true,vis:9});
     $('.page4 .info-box').slide({mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:3});
 });
